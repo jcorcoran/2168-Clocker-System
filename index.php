711b6ff04ed2228ce50df1c22d277195fe60bab3
Vittorio Papandrea <papandreavittorio@gmail.com>
2013-10-05 08:28:30 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<html>
+
+        <title>2168 Clocker</title>
+
+        <body>
+                <form name='frmMain' method='post' action="retTimes.php">
+                <input type = "text" name = "CardNumber" value = "Card Number">
+                <input type = "submit" name = "CheckTimes" value = "Check Times$
+
+
+        </body>
+
+</html>
+
