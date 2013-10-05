c0e01ad416aa11b89ac001737c0ee7a889b0e2db
Vittorio Papandrea <papandreavittorio@gmail.com>
2013-10-05 08:20:53 -0400

    Update app.php

--- app.php
+++ app.php
@@ -1,22 +1,18 @@
 <?php
-  $data = $_SERVER['QUERY_STRING'];
-  
-  list($CardNumber, $TimeSpent) = explode('=',$data);
-  
-  $filename = date('M-d-y');
-  $filename .= ".LOG";
-  $file = $filename;
-  
-  $current .= $CardNumber;
-  $current .= " ";
-  $current .= $TimeSpent;
-  $current .= "\n";
-  
-  file_put_contents($file,$current, FILE_APPEND | LOCK_EX);
+$data = $_SERVER['QUERY_STRING'];
+
+list($CardNumber, $TimeSpent) = explode('=',$data);
+
+$filename = "LOG";
+$file = $filename;
+
+$current .= $CardNumber . " " . $TimeSpent . "\n";
+
+file_put_contents($file,$current, FILE_APPEND | LOCK_EX);
 ?>
 
 <html>
-  <center>Restricted Page!</center>
-  <center>This page is restricted to the 2168 Clocker application.</center>
-  <center>Because you have visited the page data logs have been added with unwanted information.</center> 
+<center>Restricted Page!</center>
+<center>This page is restricted to the 2168 Clocker application.</center>
+<center>Because you have visited the page data logs have been added with unwant$
 </html>
