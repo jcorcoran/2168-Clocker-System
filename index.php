824722eba326ce98a0df9d46ab90cd28d7f589bb
Vittorio Papandrea <papandreavittorio@gmail.com>
2013-10-06 10:57:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,47 @@
 <?php
 
+	ini_set('auto_detect_line_endings', true);
+
+	$newline = false;
+	$lcount = 0;
+
+	echo '<center>';
+
+	echo '<table border="1">';
+	echo '<tr>';
+	echo '<th>' . "First Name" . '</th>';
+	echo '<th>' . "Last Name" . '</th>';
+	echo '<th>' . "Time Spent" . '</th>';
+	echo '</tr>';						
+
+	foreach(file("members") as $membername){
+
+		$time = 0;
+		$cn_public;
+	
+		list($firstname, $lastname, $cardnumber) = explode(" ", $membername);
+		$cn_public = rtrim($cardnumber, "\n");
+
+		foreach(file("LOG") as $data_entry){
+			list($log_cardnumber, $log_time) = explode(" ", $data_entry);
+			if($log_cardnumber === $cn_public){
+				$time += $log_time;
+			}
+		}
+
+		$time /= 60;
+		$time = round($time, 2);
+			
+		echo '<tr>';
+
+		echo '<td>' . $firstname . '</td>';
+		echo '<td>' . $lastname . '</td>';
+		echo '<td>' . $time . '</td>';	
+
+		echo '</tr>';
+			
+	}
+			
+	echo '</center>';
+
 ?>
-
-<html>
-
-        <title>2168 Clocker</title>
-
-        <body>
-                <form name='frmMain' method='post' action="retTimes.php">
-                <input type = "text" name = "CardNumber" value = "Card Number">
-                <input type = "submit" name = "CheckTimes" value = "Check Times$
-
-
-        </body>
-
-</html>
-
